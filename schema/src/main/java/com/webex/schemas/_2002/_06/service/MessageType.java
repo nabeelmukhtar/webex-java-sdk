a41dcddcdaa8835f364e50eed39196a25fd1dffb
nabeelmukhtar <nabeelmukhtar@localhost>
2011-03-02 16:05:21 +0500

    Changed JAXB library.

--- schema/src/main/java/com/webex/schemas/_2002/_06/service/MessageType.java
+++ schema/src/main/java/com/webex/schemas/_2002/_06/service/MessageType.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -55,6 +56,7 @@ import javax.xml.bind.annotation.XmlType;
     "header",
     "body"
 })
+@XmlRootElement
 public class MessageType {
 
     @XmlElement(required = true)
