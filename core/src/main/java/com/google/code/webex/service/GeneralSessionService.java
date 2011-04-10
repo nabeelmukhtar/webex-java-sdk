d76d290dc57864bf08e6c2f9b66c4acbad44c1ac
nabeelmukhtar <nabeelmukhtar@localhost>
2011-04-10 10:54:35 +0500

    Useless commit.

--- core/src/main/java/com/google/code/webex/service/GeneralSessionService.java
+++ core/src/main/java/com/google/code/webex/service/GeneralSessionService.java
@@ -18,7 +18,14 @@ package com.google.code.webex.service;
 
 import java.util.List;
 
+import com.webex.schemas._2002._06.common.AddressTypeType;
+import com.webex.schemas._2002._06.common.ServiceTypeType;
 import com.webex.schemas._2002._06.service.ep.ContactType;
+import com.webex.schemas._2002._06.service.ep.DistListInstanceType;
+import com.webex.schemas._2002._06.service.ep.LstOpenSessionResponse;
+import com.webex.schemas._2002._06.service.ep.LstsummarySession;
+import com.webex.schemas._2002._06.service.ep.RecordingType;
+import com.webex.schemas._2002._06.service.ep.SessionSummaryInstanceType;
 
 /**
  * @author nmukhtar
@@ -28,4 +35,11 @@ public interface GeneralSessionService extends WebExService {
 	public List<Long> createContacts(List<ContactType> contacts);
 	public Long createDistributionList(String name, String description, List<Long> contactIds);
 	public Integer deleteRecording(Integer recordingId);
+	public void deleteSession(Long sessionKey, boolean sendEmail);
+	public String getApiVersion();
+	public List<DistListInstanceType> getDistributionLists(Long distListID, String name, AddressTypeType addressType, String hostWebExID);
+	public List<LstOpenSessionResponse.Services> getOpenSessions(ServiceTypeType... serviceTypes);
+	// TODO-NM: Some parameters remaining
+	public List<RecordingType> getRecordings(String hostWebExID, Long sessionKey, boolean returnSessionDetails);
+	public List<SessionSummaryInstanceType> getSessionSummaries(LstsummarySession sessionRequest);
 }
--- core/src/main/java/com/google/code/webex/service/impl/GeneralSessionServiceImpl.java
+++ core/src/main/java/com/google/code/webex/service/impl/GeneralSessionServiceImpl.java
@@ -23,6 +23,8 @@ import com.google.code.webex.service.GeneralSessionService;
 import com.google.code.webex.service.util.ApplicationConstants;
 import com.google.code.webex.service.util.WebExUrls;
 import com.google.code.webex.service.util.WebExUrls.WebExUrlBuilder;
+import com.webex.schemas._2002._06.common.AddressTypeType;
+import com.webex.schemas._2002._06.common.ServiceTypeType;
 import com.webex.schemas._2002._06.service.BodyContentType;
 import com.webex.schemas._2002._06.service.MessageType;
 import com.webex.schemas._2002._06.service.ep.ContactType;
@@ -32,8 +34,24 @@ import com.webex.schemas._2002._06.service.ep.CreateDistList;
 import com.webex.schemas._2002._06.service.ep.CreateDistListResponse;
 import com.webex.schemas._2002._06.service.ep.DelRecording;
 import com.webex.schemas._2002._06.service.ep.DelRecordingResponse;
+import com.webex.schemas._2002._06.service.ep.DelSession;
+import com.webex.schemas._2002._06.service.ep.DistListInstanceType;
 import com.webex.schemas._2002._06.service.ep.DistListWithContactType;
+import com.webex.schemas._2002._06.service.ep.GetAPIVersion;
+import com.webex.schemas._2002._06.service.ep.GetAPIVersionResponse;
+import com.webex.schemas._2002._06.service.ep.ListControlType;
+import com.webex.schemas._2002._06.service.ep.LstDistList;
+import com.webex.schemas._2002._06.service.ep.LstDistListResponse;
+import com.webex.schemas._2002._06.service.ep.LstOpenSession;
+import com.webex.schemas._2002._06.service.ep.LstOpenSessionResponse;
+import com.webex.schemas._2002._06.service.ep.LstRecording;
+import com.webex.schemas._2002._06.service.ep.LstRecordingResponse;
+import com.webex.schemas._2002._06.service.ep.LstsummarySession;
+import com.webex.schemas._2002._06.service.ep.LstsummarySessionResponse;
 import com.webex.schemas._2002._06.service.ep.ObjectFactory;
+import com.webex.schemas._2002._06.service.ep.RecordingType;
+import com.webex.schemas._2002._06.service.ep.SessionSummaryInstanceType;
+import com.webex.schemas._2002._06.service.ep.LstOpenSessionResponse.Services;
 
 public class GeneralSessionServiceImpl extends WebExJaxbService implements
 		GeneralSessionService {
@@ -94,5 +112,113 @@ public class GeneralSessionServiceImpl extends WebExJaxbService implements
 			return ((DelRecordingResponse) bodyContents.get(0)).getRecordingID();			
 		}
 		return null;
+	}
+
+	@Override
+	public void deleteSession(Long sessionKey, boolean sendEmail) {
+		WebExUrlBuilder builder = createWebExUrlBuilder(WebExUrls.API_URL);
+		ObjectFactory factory = new ObjectFactory();
+		DelSession delSession = factory.createDelSession();
+		delSession.setSessionKey(sessionKey);
+		delSession.setSendEmail(sendEmail);
+		MessageType request = createRequest(delSession);
+		MessageType response = unmarshallObject(MessageType.class, callApiMethod(builder.buildUrl(), marshallObject(request), ApplicationConstants.CONTENT_TYPE_XML, HttpMethod.POST, 200));
+
+		List<BodyContentType> bodyContents = response.getBody().getBodyContent();
+		if (!bodyContents.isEmpty()) {
+		}
+	}
+
+	@Override
+	public String getApiVersion() {
+		WebExUrlBuilder builder = createWebExUrlBuilder(WebExUrls.API_URL);
+		ObjectFactory factory = new ObjectFactory();
+		GetAPIVersion getApiVersion = factory.createGetAPIVersion();
+		MessageType request = createRequest(getApiVersion);
+		MessageType response = unmarshallObject(MessageType.class, callApiMethod(builder.buildUrl(), marshallObject(request), ApplicationConstants.CONTENT_TYPE_XML, HttpMethod.POST, 200));
+
+		List<BodyContentType> bodyContents = response.getBody().getBodyContent();
+		if (!bodyContents.isEmpty()) {
+			return ((GetAPIVersionResponse) bodyContents.get(0)).getApiVersion();			
+		}
+		
+		return null;
+	}
+
+	@Override
+	public List<DistListInstanceType> getDistributionLists(Long distListID,
+			String name, AddressTypeType addressType, String hostWebExID) {
+		WebExUrlBuilder builder = createWebExUrlBuilder(WebExUrls.API_URL);
+		ObjectFactory factory = new ObjectFactory();
+		LstDistList distList = factory.createLstDistList();
+		distList.setDistListID(distListID);
+		distList.setName(name);
+		distList.setAddressType(addressType);
+		distList.setHostWebExID(hostWebExID);
+		MessageType request = createRequest(distList);
+		MessageType response = unmarshallObject(MessageType.class, callApiMethod(builder.buildUrl(), marshallObject(request), ApplicationConstants.CONTENT_TYPE_XML, HttpMethod.POST, 200));
+
+		List<BodyContentType> bodyContents = response.getBody().getBodyContent();
+		if (!bodyContents.isEmpty()) {
+			return ((LstDistListResponse) bodyContents.get(0)).getDistList();			
+		}
+		
+		return new ArrayList<DistListInstanceType>();
+	}
+
+	@Override
+	public List<Services> getOpenSessions(ServiceTypeType... serviceTypes) {
+		WebExUrlBuilder builder = createWebExUrlBuilder(WebExUrls.API_URL);
+		ObjectFactory factory = new ObjectFactory();
+		LstOpenSession distSession = factory.createLstOpenSession();
+		for (int i = 0; i < serviceTypes.length; i++) {
+			distSession.getServiceType().add(serviceTypes[i]);
+		}
+		MessageType request = createRequest(distSession);
+		MessageType response = unmarshallObject(MessageType.class, callApiMethod(builder.buildUrl(), marshallObject(request), ApplicationConstants.CONTENT_TYPE_XML, HttpMethod.POST, 200));
+
+		List<BodyContentType> bodyContents = response.getBody().getBodyContent();
+		if (!bodyContents.isEmpty()) {
+			return ((LstOpenSessionResponse) bodyContents.get(0)).getServices();			
+		}
+		
+		return new ArrayList<Services>();
+	}
+
+	@Override
+	public List<RecordingType> getRecordings(String hostWebExID,
+			Long sessionKey, boolean returnSessionDetails) {
+		WebExUrlBuilder builder = createWebExUrlBuilder(WebExUrls.API_URL);
+		ObjectFactory factory = new ObjectFactory();
+		LstRecording lstRecordings = factory.createLstRecording();
+		ListControlType listControl = factory.createListControlType();
+		lstRecordings.setListControl(listControl);
+		listControl.setMaximumNum(5L);
+		
+		lstRecordings.setHostWebExID(hostWebExID);
+		lstRecordings.setSessionKey(sessionKey);
+		lstRecordings.setReturnSessionDetails(returnSessionDetails);
+		
+		MessageType request = createRequest(lstRecordings);
+		MessageType response = unmarshallObject(MessageType.class, callApiMethod(builder.buildUrl(), marshallObject(request), ApplicationConstants.CONTENT_TYPE_XML, HttpMethod.POST, 200));
+		
+		List<BodyContentType> bodyContents = response.getBody().getBodyContent();
+		if (!bodyContents.isEmpty()) {
+			return ((LstRecordingResponse) bodyContents.get(0)).getRecording();			
+		}
+		return new ArrayList<RecordingType>();
+	}
+
+	@Override
+	public List<SessionSummaryInstanceType> getSessionSummaries(LstsummarySession sessionRequest) {
+		WebExUrlBuilder builder = createWebExUrlBuilder(WebExUrls.API_URL);
+		MessageType request = createRequest(sessionRequest);
+		MessageType response = unmarshallObject(MessageType.class, callApiMethod(builder.buildUrl(), marshallObject(request), ApplicationConstants.CONTENT_TYPE_XML, HttpMethod.POST, 200));
+		
+		List<BodyContentType> bodyContents = response.getBody().getBodyContent();
+		if (!bodyContents.isEmpty()) {
+			return ((LstsummarySessionResponse) bodyContents.get(0)).getSession();			
+		}
+		return new ArrayList<SessionSummaryInstanceType>();
 	}
 }
