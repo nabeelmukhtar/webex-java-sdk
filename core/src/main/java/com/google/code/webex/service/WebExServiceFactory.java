ea22ad4e26f86f93ff849315c1582b6e4908085c
nabeelmukhtar <nabeelmukhtar@localhost>
2011-04-21 20:59:18 +0500

    JAutoDoc

--- core/src/main/java/com/google/code/webex/service/WebExServiceFactory.java
+++ core/src/main/java/com/google/code/webex/service/WebExServiceFactory.java
@@ -21,7 +21,17 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.google.code.webex.service.impl.EventSessionServiceImpl;
+import com.google.code.webex.service.impl.GeneralSessionServiceImpl;
+import com.google.code.webex.service.impl.HistoryServiceImpl;
+import com.google.code.webex.service.impl.MeetingAttendeeServiceImpl;
 import com.google.code.webex.service.impl.MeetingServiceImpl;
+import com.google.code.webex.service.impl.MeetingTypeServiceImpl;
+import com.google.code.webex.service.impl.SalesSessionServiceImpl;
+import com.google.code.webex.service.impl.SiteServiceImpl;
+import com.google.code.webex.service.impl.SupportSessionServiceImpl;
+import com.google.code.webex.service.impl.TrainingSessionServiceImpl;
+import com.google.code.webex.service.impl.UserServiceImpl;
 
 /**
  * A factory for creating WebExService objects.
@@ -96,5 +106,95 @@ public class WebExServiceFactory {
      */
 	public MeetingService createMeetingService() {
 		return new MeetingServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the event session service
+	 */
+	public EventSessionService createEventSessionService() {
+		return new EventSessionServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the general session service
+	 */
+	public GeneralSessionService createGeneralSessionService() {
+		return new GeneralSessionServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the history service
+	 */
+	public HistoryService createHistoryService() {
+		return new HistoryServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the meeting attendee service
+	 */
+	public MeetingAttendeeService createMeetingAttendeeService() {
+		return new MeetingAttendeeServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the meeting type service
+	 */
+	public MeetingTypeService createMeetingTypeService() {
+		return new MeetingTypeServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the sales session service
+	 */
+	public SalesSessionService createSalesSessionService() {
+		return new SalesSessionServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the site service
+	 */
+	public SiteService createSiteService() {
+		return new SiteServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the support session service
+	 */
+	public SupportSessionService createSupportSessionService() {
+		return new SupportSessionServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the training session service
+	 */
+	public TrainingSessionService createTrainingSessionService() {
+		return new TrainingSessionServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
+    }
+	
+	/**
+	 * Creates a new WebExService object.
+	 * 
+	 * @return the user service
+	 */
+	public UserService createUserService() {
+		return new UserServiceImpl(apiConsumer.getWebExId(), apiConsumer.getPassword(), apiConsumer.getSiteId(), apiConsumer.getSiteName(), apiConsumer.getPartnerId());
     }
 }
